ce6a33e0554f9ad77dbc7cec63017fe19a4865de
danilopking <danil.lopatkin1@gmail.com>
2025-08-31 13:27:30 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+
+const CACHE = "42-fitness-v9";
+const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => {
+  e.waitUntil((async () => {
+    const names = await caches.keys();
+    await Promise.all(names.filter(n => n !== CACHE).map(n => caches.delete(n)));
+    self.clients.claim();
+  })());
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  }
+});
